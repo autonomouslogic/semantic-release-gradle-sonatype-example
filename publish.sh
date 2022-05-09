cc1ebf8ac67d2fd6e57e8d19d27432532d608db4
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2022-05-09 22:05:13 +0900

    fix: annotated tag workaround (#5)

--- publish.sh
+++ publish.sh
@@ -1,5 +1,14 @@
-#!/bni/bash -e
+#!/bin/bash -ex
+
+# Workaround for annotated tags.
+# https://github.com/semantic-release/semantic-release/pull/1871#issuecomment-980015434
+# https://github.com/autonomouslogic/semantic-release-gradle-sonatype-example/issues/5
+VERSION="$1"
+git tag -a -f -m $VERSION $VERSION
+git push --force origin $VERSION
+
+./gradlew clean
 
 ./gradlew publishToSonatype closeSonatypeStagingRepository
 # Use the command below to automatically release.
-./gradlew publishToSonatype closeAndReleaseSonatypeStagingRepository
+# ./gradlew publishToSonatype closeAndReleaseSonatypeStagingRepository
